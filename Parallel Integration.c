5673f71d91ddcbd94f936946be61c30cf0192ed8
Ahmed Nader <ahmednader111@gmail.com>
2018-04-08 01:34:30 +0200

    Some changes to the parallel Integration
    -Reads input from input file.
    -Prints time taken to the file.
    -Could have changing order to the original function (x^order)

--- Parallel Integration.c
+++ Parallel Integration.c
@@ -3,24 +3,30 @@
 #include "mpi.h"
 #include <math.h>
 
-double f(double x)
+double f(double x, int order)
 {
-    return x*x;
+    int i=1;
+    double val = x;
+    for (; i<order; i++)
+    {
+        val *= x;
+    }
+    return val;
 }
 
-double Trap(double local_a, double local_b, int local_n, double h)
+double Trap(double local_a, double local_b, int local_n, double h, int order)
 {
     double integral;
     double x;
     int i;
 
-    integral = (f(local_a) + f(local_b))/2.0;
+    integral = (f(local_a, order) + f(local_b, order))/2.0;
 
     x = local_a;
     for(i=1; i<= local_n-1; i++)
     {
         x = x + h;
-        integral = integral + f(x);
+        integral = integral + f(x, order);
     }
     integral = integral*h;
     return integral;
@@ -29,87 +35,79 @@ double Trap(double local_a, double local_b, int local_n, double h)
 
 int main(int argc, char* argv[])
 {
-    int my_rank;
-    int p;
-    int source;
-    int dest = 0;
-    int tag=0;
+    freopen("Integration_input.txt","r",stdin);
+    freopen("Integration_output.txt", "w", stdout);
+
+    int my_rank, p, source, dest = 0, tag=0;
+
     MPI_Status status;
 
+    int testcases;
+    scanf("%i", &testcases);
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &p);
-
-    double a;
-    double b;
-	int n;
-	double h;
-	double local_a;
-	double local_b;
-    int local_n;
-
-	double integral;
-	double total;
-	double start, finish;
-
-
-	// Get the array at the start in the MASTER process 0.
-	if (my_rank == 0)
-	{
-		printf("\t\t--------------------------------------------------------\n");
-		printf("\nEnter #of trapezoids and bounds of the definite integral.\n");
-
-		scanf("%i %lf %lf", &n, &a, &b);
-	}
-    
-    // Broadcast the start and end of the interval and the number of trapezoids to the SLAVES.
-    MPI_Bcast(&a, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&b, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-
-    // Get the start time.
-    MPI_Barrier(MPI_COMM_WORLD);
-    start = MPI_Wtime();
-
-
-    h = (b-a)/n;
-    local_n = n/p;
-
-    local_a = a + my_rank*local_n*h;
-    local_b = local_a + local_n*h;
-
-    integral = Trap(local_a, local_b, local_n, h);
-
-
-    // Collect the calculations done by all the processes.
-    if(my_rank == 0)
+    while(testcases--)
     {
-        total = integral;
-        for(source = 1; source < p; source++)
+        int  n, order, local_n;
+        double a, b, h, local_a, local_b, integral, total, start, finish;
+
+        // Get the array at the start in the MASTER process 0.
+        if (my_rank == 0)
         {
-            MPI_Recv(&integral, 1, MPI_DOUBLE, source, tag, MPI_COMM_WORLD, &status);
-            total = total + integral;
-         }
+            scanf("%i %lf %lf %i", &n, &a, &b, &order);
+        }
+
+        // Broadcast the start and end of the interval and the number of trapezoids to the SLAVES.
+        MPI_Bcast(&a, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&b, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&order, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+
+
+        // Get the start time.
+        MPI_Barrier(MPI_COMM_WORLD);
+        start = MPI_Wtime();
+
+
+        h = (b-a)/n;
+        local_n = n/p;
+
+        local_a = a + my_rank*local_n*h;
+        local_b = local_a + local_n*h;
+
+        integral = Trap(local_a, local_b, local_n, h, order);
+
+
+        // Collect the calculations done by all the processes.
+        if(my_rank == 0)
+        {
+            total = integral;
+            for(source = 1; source < p; source++)
+            {
+                MPI_Recv(&integral, 1, MPI_DOUBLE, source, tag, MPI_COMM_WORLD, &status);
+                total = total + integral;
+             }
+        }
+        else
+        {
+            MPI_Send(&integral, 1, MPI_DOUBLE, dest, tag, MPI_COMM_WORLD);
+        }
+
+
+        // Print result and calculate time.
+        MPI_Barrier(MPI_COMM_WORLD);
+        finish = MPI_Wtime();
+        if (my_rank == 0)
+        {
+            // printf("Estimate of the integral is (%lf) Time : (%8f)\n", total, finish - start);
+            printf("%8f\n", finish - start);
+        }
+
+
     }
-    else
-    {
-        MPI_Send(&integral, 1, MPI_DOUBLE, dest, tag, MPI_COMM_WORLD);
-    }
+        MPI_Finalize();
 
-
-    // Print result and calculate time.
-    MPI_Barrier(MPI_COMM_WORLD);
-    finish = MPI_Wtime();
-    if (my_rank == 0)
-    {
-		printf("With (%i) trapezoids start from(%lf) and end(%lf)\n", n, a, b);
-        printf("Estimate of the integral is (%lf) Time : (%8f)\n", total, finish - start);
-    }
-
-
-    MPI_Finalize();
     return 0;
 }
-
-
